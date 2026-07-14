cb530f06b9ab966f5f18a48863119cd0143577f5
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-07-14 14:49:10 +0530

    Add files via upload

---
+++ FastAPI/Edutrack - Student Information Management System/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/student_management"
+
+engine = create_engine(
+    DATABASE_URL
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
